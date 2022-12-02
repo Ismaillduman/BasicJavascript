727b53cb237b6c8cef412f07bd19b1932bd9e1e9
Issmailduman <https://github.com/Issmailduman>
2022-12-03 00:57:58 +0100

    for loop

--- basic2.js
+++ basic2.js
@@ -16,3 +16,21 @@ do{
     i++
 } while(i>10);
 console.log(i)
+console.log("+++++++++++++++++++++++++++++++")
+// for loop from 1 to 10 give me common multiple values of 2 and 5
+let n= 0
+for(let k=1;k<=100;k++){
+if(k%2 == 0 && k%5 == 0){
+n++
+    console.log(k)
+if(n==3)
+break
+
+}
+}
+let required = true
+while(required){
+   
+console.log(required)
+required= false
+}
