a9075a5e5f63fd333ea450dcd9b69e46039f4e52
Issmailduman <https://github.com/Issmailduman>
2022-12-02 22:39:25 +0100

    First commit

---
+++ basic1.js
@@ -0,0 +1,27 @@
+console.log("Hello world");
+// these will not executed
+/*
+Three type of variables 
+var
+let
+const
+*/
+let a =4
+let b = 4.56
+console.log(a);
+console.log(typeof(a))
+console.log(b)
+console.log(typeof(b))
+
+//var c = "IsmailDuman"
+let c = "IsmailDuman"
+console.log(typeof(c))
+let required = true
+console.log(typeof(required))
+//var c =a+b
+ c= a+b
+console.log(c)
+//we can not redeclare with let keyword but that’s possible with var keyword.
+//But we can reassigned with let keyword
+
+console.log(!required)
