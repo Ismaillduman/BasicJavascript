f2890be537f82985fdca85bd1b48448e4ae77351
Issmailduman <https://github.com/Issmailduman>
2022-12-03 20:50:35 +0100

    sorting

---
+++ sorting.js
@@ -0,0 +1,8 @@
+var fruits = ["mango", "apple", "domaten", "banana"];
+console.log(fruits.sort());
+console.log(fruits.reverse());
+
+var scores=[12,003,13,19,11]
+
+console.log(scores.sort((a,b)=> a-b))
+console.log(scores.sort((a,b)=> b-a))
