3eeef16440d654fe89ebb980469fdb33d91db7ee
Issmailduman <https://github.com/Issmailduman>
2022-12-03 22:18:09 +0100

    FunctionChange

--- function.js
+++ function.js
@@ -1,4 +1,13 @@
-function add(){
-
-    
+function add(a, b) {
+  return a + b;
 }
+let sum = add(2, 3);
+console.log(sum);
+
+let sumOfIntegers=function(c,d){
+    return c+d
+}
+let sumOfNumbers=(c,d)=>{return c+d}
+
+let sumOfMbr = (c,d)=> c+d // easy way
+console.log(sumOfMbr(2,3))
